a36e0ef27c5b8280a230628377d4004a9462d63a
wanhar <wanhar@sehatq.com>
2020-08-07 20:35:27 +0700

    Initial Commit

---
+++ SesamaMuslim/SesamaMuslimApp.swift
@@ -0,0 +1,17 @@
+//
+//  SesamaMuslimApp.swift
+//  SesamaMuslim
+//
+//  Created by Wanhar on 07/08/20.
+//
+
+import SwiftUI
+
+@main
+struct SesamaMuslimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
